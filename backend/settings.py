77681c75313a5717f331e5ccbc4c9ee6e6938c36
patrykwys98 <patrykwys98@gmail.com>
2022-04-11 23:34:27 +0200

    Repair Whitenoise

--- backend/settings.py
+++ backend/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 ROOT_URLCONF = 'backend.urls'
 
